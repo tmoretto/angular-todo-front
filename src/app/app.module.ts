14ea04d4e5aad3c406996532895814b797e9cfa5
Tiago Moretto <tiagomoretto@Tiagos-MacBook-Pro.local>
2017-11-12 18:39:11 -0200

    Criando projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent } from './app.component';
 
 
+import { TodoListComponent } from './todo-list.component';
+
+import { TodoService } from './todo.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
